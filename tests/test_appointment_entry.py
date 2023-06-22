f3233d808f60caec02f2d8f63128a6a936973af2
Sirinya Richardson <siri.richardson@gmail.com>
2023-06-23 00:10:37 +0200

    tests: creating appointment entry

---
+++ tests/test_appointment_entry.py
@@ -0,0 +1,52 @@
+from datetime import time
+
+import pytest
+
+from django.contrib.auth import get_user_model
+
+from journal.models import AppointmentEntry
+
+User = get_user_model()
+
+
+@pytest.fixture
+def user():
+    """
+    Fixture for creating a user object.
+    """
+    return User.objects.create_user(
+        email="normal@user.com",
+        password="abcdefghij123!+_",
+        first_name="Normal",
+        last_name="User"
+    )
+
+
+@pytest.mark.django_db
+def test_create_appointment_entry(user):
+    """
+    GIVEN an appointment entry model
+    WHEN creating an appointment entry
+    THEN user should have successfully created an appointment entry
+    """
+    appointment_entry = AppointmentEntry.objects.create(
+        user=user,
+        title="Dentist",
+        date="2023-07-06",
+        time_from=time(10, 0),
+        time_until=time(11, 0)
+    )
+    appointment_entry.save()
+    appointment_entries = AppointmentEntry.objects.all()
+    assert len(appointment_entries) == 1
+    assert appointment_entries[0].user == user
+    assert appointment_entries[0].title == "Dentist"
+    assert appointment_entries[0].date == "2023-07-06"
+    assert isinstance(
+        appointment_entries[0].date, str) and appointment_entries[0].date is not None
+    assert appointment_entries[0].time_from == time(10, 0)
+    assert isinstance(appointment_entries[0].time_from,
+                      time) and appointment_entries[0].time_from is not None
+    assert appointment_entries[0].time_until == time(11, 0)
+    assert isinstance(appointment_entries[0].time_until,
+                      time) and appointment_entries[0].time_until is not None

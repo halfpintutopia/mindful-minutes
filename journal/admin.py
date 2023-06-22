145e813d877f77d54fed255df16c56a9e34a7cfc
Sirinya Richardson <siri.richardson@gmail.com>
2023-06-23 01:02:10 +0200

    build: add the appointment entry to admin

--- journal/admin.py
+++ journal/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser, UserSettings
+from .models import CustomUser, UserSettings, AppointmentEntry
 
 
 class CustomUserAdmin(UserAdmin):
@@ -51,6 +51,7 @@ class UserSettingsAdmin(admin.ModelAdmin):
 
     The class defines the display and behaviour of the UserSettings model
     """
+    model = UserSettings
     readonly_fields = (
         "created_date",
         "updated_date"
@@ -67,6 +68,33 @@ class UserSettingsAdmin(admin.ModelAdmin):
         "morning_check_in",
         "evening_check_in"
     )
+
+
+@admin.register(AppointmentEntry)
+class AppointmentEntryAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the AppointmentEntry model
+
+    The class defines the display and behaviour fo the AppointmentEntry model
+    """
+    readonly_fields = (
+        "created_on",
+        "updated_on"
+    )
+    list_display = (
+        "user",
+        "title",
+        "date",
+        "time_from",
+        "time_until"
+    )
+    fieldsets = (
+        ("Owner", {"fields": ("user",)}),
+        ("Title", {"fields": ("title",)}),
+        ("Date & Time", {"fields": ("date", "time_from", "time_until")})
+    )
+    search_fields = ["user", "title", "date"]
+    ordering = ("date",)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)

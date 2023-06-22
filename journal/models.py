d9743004697e27ce2c2f34e38e9d4f546086864d
Sirinya Richardson <siri.richardson@gmail.com>
2023-06-23 00:38:26 +0200

    build: add appointment entry model

--- journal/models.py
+++ journal/models.py
@@ -52,3 +52,39 @@ class UserSettings(models.Model):
     evening_check_in = models.TimeField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+
+class AppointmentEntry(models.Model):
+    """
+    Appointment entry model to allow users to create appointment entries
+    """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="appointment_entries"
+    )
+    title = models.CharField(_("Title"), max_length=255)
+    date = models.DateField(_("Date"))
+    time_from = models.TimeField(_("From"))
+    time_until = models.TimeField(_("Until"))
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """_
+        Meta options for the AppointmentEntry model
+        """
+        ordering = ["-date", "-time_from"]
+
+    def clean(self):
+        """
+        Custom validation to ensure the time_from is less than the time_until
+        """
+        if self.time_from > self.time_until:
+            raise ValidationError(
+                _("'From' must start before the time set on 'Until'.")
+            )
+        return super().clean()
+
+    def __str__(self):
+        return self.title

898909ae514aa70be8a4625a0d0b23c8812cf929
Sirinya Richardson <siri.richardson@gmail.com>
2023-06-23 22:08:54 +0200

    build: add emotion entry model

--- journal/models.py
+++ journal/models.py
@@ -246,3 +246,34 @@ class ImprovementEntry(models.Model):
         """
         verbose_name_plural = "Improvement Entries"
         ordering = ["created_on"]
+
+
+class EmotionEntry(models.Model):
+    """
+    EmotionEntry model to allow users to create improvement entries
+    """
+    EMOTION_CHOICES = [
+        ("awful", "Awful"),
+        ("terrible", "Terrible"),
+        ("bad", "Bad"),
+        ("okay", "Okay"),
+        ("good", "Good"),
+        ("great", "Great"),
+        ("excellent", "Excellent"),
+    ]
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="emotion_entries"
+    )
+    emotion = models.CharField(
+        _("Emotion Entry"), max_length=10, choices=EMOTION_CHOICES)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """
+        Meta options for the EmotionEntry model
+        """
+        verbose_name_plural = "Emotion Entries"
+        ordering = ["created_on"]

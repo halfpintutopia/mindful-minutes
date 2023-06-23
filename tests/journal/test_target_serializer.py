a1eed469f64ccba7b64c7c417d0b610f704f41de
Sirinya Richardson <siri.richardson@gmail.com>
2023-06-23 12:52:59 +0200

    tests: add target serializer

---
+++ tests/journal/test_target_serializer.py
@@ -0,0 +1,34 @@
+from journal.serializers import TargetSerializer
+
+
+def test_valid_target_serializer():
+    """
+    GIVEN a valid target serializer
+    WHEN the data us passed to the serializer
+    THEN the serializer should be valid
+    """
+    valid_serializer_data = {
+        "title": "Mediate for 20 minutes",
+        "order": 1,
+    }
+    serializer = TargetSerializer(data=valid_serializer_data)
+    assert serializer.is_valid
+    assert serializer.validated.data == valid_serializer_data
+    assert serializer.data == valid_serializer_data
+    assert not serializer.errors
+
+
+def test_invalid_missing_title_target_serializer():
+    """
+    GIVEN an invalid target serializer with missing title
+    WHEN the data is passed to the serializer
+    THEN the serializer should be invalid
+    """
+    invalid_serializer_data = {
+        "order": 1
+    }
+    serializer = TargetSerializer(data=invalid_serializer_data)
+    assert not serializer.is_valid()
+    assert serializer.validated_data == {}
+    assert serializer.data == invalid_serializer_data
+    assert serializer.errors == {"title": ["This field is required"]}

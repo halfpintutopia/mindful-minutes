e33c794c8549be7adabe4a5282c47d2937deb120
Sirinya Richardson <siri.richardson@gmail.com>
2023-06-23 10:58:08 +0200

    chore: refactor name for user settings serializer

--- tests/journal/test_user_settings_serializers.py
+++ tests/journal/test_user_settings_serializers.py
@@ -29,7 +29,7 @@ def test_valid_user_setting_serializer():
         "morning_check_in": time(8, 30).strftime("%H:%M:%S"),
         "evening_check_in": time(20, 0).strftime("%H:%M:%S")
     }
-    assert serializer.errors == {}
+    assert not serializer.errors
 
 
 @pytest.mark.django_db
@@ -45,10 +45,7 @@ def test_invalid_user_settings_serializer_missing_start_week_day():
     }
     serializer = UserSettingsSerializer(data=invalid_serializer_data)
     assert not serializer.is_valid()
-
-    print("validated_data", invalid_serializer_data)
-    print("data", serializer.data)
-
+    assert serializer.validated_data == {}
     assert serializer.data == invalid_serializer_data
     assert serializer.errors == {
         "start_week_day": ["This field is required."]
@@ -68,7 +65,11 @@ def test_invalid_user_settings_serializer_missing_morning_check_in():
     }
     serializer = UserSettingsSerializer(data=invalid_serializer_data)
     assert not serializer.is_valid()
-    assert serializer.data == invalid_serializer_data
+    assert serializer.validated_data == {}
+    assert serializer.data == {
+        "start_week_day": 1,
+        "evening_check_in": time(20, 00)
+    }
     assert serializer.errors == {
         "morning_check_in": ["This field is required."]
     }
@@ -87,6 +88,7 @@ def test_invalid_user_settings_serializer_missing_evening_check_in():
     }
     serializer = UserSettingsSerializer(data=invalid_serializer_data)
     assert not serializer.is_valid()
+    assert serializer.validated_data == {}
     assert serializer.data == invalid_serializer_data
     assert serializer.errors == {
         "evening_check_in": ["This field is required."]

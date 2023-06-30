c225cbc30c092431dacc74cfb2e7a6971631fcb4
Sirinya Richardson <siri.richardson@gmail.com>
2023-06-30 19:59:58 +0200

    tests: DRF views for notes

--- journal/urls.py
+++ journal/urls.py
@@ -3,22 +3,37 @@ from django.urls import path
 from .api_views.appointment_entries import AppointmentEntryList, \
     AppointmentEntryDetail
 from .api_views.target_entries import TargetEntryList, TargetEntryDetail
+from .api_views.note_entries import NoteEntryList, NoteEntryDetail
 
 urlpatterns = [
     path(
         "api/appointments/<str:date_request>/<int:pk>/",
-        AppointmentEntryDetail.as_view()
+        AppointmentEntryDetail.as_view(),
+        name="appointment-entry-detail"
     ),
     path(
         "api/appointments/<str:date_request>/",
-        AppointmentEntryList.as_view()
+        AppointmentEntryList.as_view(),
+        name="appointment-entry-list"
     ),
     path(
-        "api/appointments/<str:date_request>/<int:pk>/",
-        TargetEntryDetail.as_view()
+        "api/targets/<str:date_request>/<int:pk>/",
+        TargetEntryDetail.as_view(),
+        name="target-entry-detail"
     ),
     path(
-        "api/appointments/<str:date_request>/",
-        TargetEntryList.as_view()
+        "api/targets/<str:date_request>/",
+        TargetEntryList.as_view(),
+        name="target-entry-list"
+    ),
+    path(
+        "api/notes/<str:date_request>/<int:pk>/",
+        NoteEntryDetail.as_view(),
+        name="note-entry-detail"
+    ),
+    path(
+        "api/notes/<str:date_request>/",
+        NoteEntryList.as_view(),
+        name="note-entry-list"
     ),
 ]

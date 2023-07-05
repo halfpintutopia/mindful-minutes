be1b3d379761b8acf8c03af56042dcb5c5e34ee6
Sirinya Richardson <siri.richardson@gmail.com>
2023-07-05 21:52:00 +0200

    test: update note entry serializer

--- tests/journal/test_note_serializer.py
+++ tests/journal/test_note_serializer.py
@@ -1,20 +1,49 @@
-from journal.serializers import NoteSerializer
+import pytest
+
+from faker import Faker
+
+from django.contrib.auth import get_user_model
+
+from journal.serializers import NoteEntrySerializer
+from journal.models import NoteEntry
+
+User = get_user_model()
+fake = Faker()
 
 
+@pytest.mark.django_db
 def test_valid_note_serializer():
     """
     GIVEN a valid note serializer
     WHEN the data us passed to the serializer
     THEN the serializer should be valid
     """
-    valid_serializer_data = {
-        "content": "I must order 'Eloquent JavaScript'",
-    }
-    serializer = NoteSerializer(data=valid_serializer_data)
-    assert serializer.is_valid()
-    assert serializer.validated_data == valid_serializer_data
-    assert serializer.data == valid_serializer_data
-    assert not serializer.errors
+    email = fake.email()
+    password = fake.password(length=16)
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+
+    user = User.objects.create_user(
+        email=email,
+        password=password,
+        first_name=first_name,
+        last_name=last_name
+    )
+
+    content = "I must order 'Eloquent JavaScript'"
+
+    note_entry = NoteEntry.objects.create(
+        user=user,
+        content=content
+    )
+
+    serializer = NoteEntrySerializer(note_entry)
+    deserialized_serializer = NoteEntrySerializer(data=serializer.data)
+    assert deserialized_serializer.is_valid()
+
+    assert deserialized_serializer["content"].value == content
+    assert serializer.data.get("content") == content
+    assert not deserialized_serializer.errors
 
 
 def test_invalid_missing_content_note_serializer():
@@ -24,7 +53,7 @@ def test_invalid_missing_content_note_serializer():
     THEN the serializer should be invalid
     """
     invalid_serializer_data = {}
-    serializer = NoteSerializer(data=invalid_serializer_data)
+    serializer = NoteEntrySerializer(data=invalid_serializer_data)
     assert not serializer.is_valid()
     assert serializer.validated_data == {}
     assert serializer.data == invalid_serializer_data

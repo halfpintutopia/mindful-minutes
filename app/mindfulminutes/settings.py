dffcf5145ff3a4a087af6c8666f1cee5fe13b014
Sirinya Richardson <siri.richardson@gmail.com>
2023-07-28 12:15:39 +0200

    refactor: quality check of settings

--- app/mindfulminutes/settings.py
+++ app/mindfulminutes/settings.py
@@ -193,7 +193,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 # Replace 'your_cloudinary_url_here' with your actual Cloudinary URL
-CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
+CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 # Default primary key field type

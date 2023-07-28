669764ab00ef0422047fc700f647fc1d3b267ffe
Sirinya Richardson <siri.richardson@gmail.com>
2023-07-28 12:57:55 +0200

    chore: add individual args for CLOUDINARY

--- app/mindfulminutes/settings.py
+++ app/mindfulminutes/settings.py
@@ -192,12 +192,12 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Replace 'your_cloudinary_url_here' with your actual Cloudinary URL
-# CLOUDINARY_STORAGE = {
-#     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-#     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-#     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-# }
-CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+}
+# CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
 MEDIA_URL = "/media/"
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 

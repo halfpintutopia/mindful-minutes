299ba1e9b2797043413ff6dbe065b9add9a1c92f
Sirinya Richardson <siri.richardson@gmail.com>
2023-07-28 11:21:49 +0200

    refactor: update settings after quality check

--- app/mindfulminutes/settings.py
+++ app/mindfulminutes/settings.py
@@ -151,19 +151,19 @@ else:
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".UserAttributeSimilarityValidator",
+        ".UserAttributeSimilarityValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".MinimumLengthValidator",
+        ".MinimumLengthValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".CommonPasswordValidator",
+        ".CommonPasswordValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".NumericPasswordValidator",
+        ".NumericPasswordValidator",
     },
 ]
 

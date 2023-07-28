731c9cddd2f7169d93610c936580f5493a02432d
Sirinya Richardson <siri.richardson@gmail.com>
2023-07-28 14:43:33 +0200

    tests: refactor to run locally

--- app/mindfulminutes/urls.py
+++ app/mindfulminutes/urls.py
@@ -19,33 +19,26 @@ import os
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Mindful Minutes API",
+        default_version="v1",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("journal.urls")),
+    path("", include("journal.api_testing.urls")),
+    path(
+        "swagger-docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
-
-development = os.environ.get("DEVELOPMENT", False)
-
-if development:
-    from drf_yasg import openapi
-    from drf_yasg.views import get_schema_view
-
-    schema_view = get_schema_view(
-        openapi.Info(
-            title="Mindful Minutes API",
-            default_version="v1",
-        ),
-        public=True,
-        permission_classes=(permissions.AllowAny,),
-    )
-
-    urlpatterns += [
-        path("", include("journal.api_testing.urls")),
-        path(
-            "swagger-docs/",
-            schema_view.with_ui("swagger", cache_timeout=0),
-            name="schema-swagger-ui",
-        ),
-    ]

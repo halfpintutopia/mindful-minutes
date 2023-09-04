a2056bfeeb901b705667d6b909adc340becaad96
Sirinya Richardson <siri.richardson@gmail.com>
2023-09-04 17:52:53 +0200

    style: add style design preview page

--- app/journal/urls.py
+++ app/journal/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from .views.index_views import index
+from .views.index_views import design_system
 
 urlpatterns = [
     path("", index, name="home"),
+    path("design-system", design_system, name="design-systems"),
 ]
--- app/journal/views/index_views.py
+++ app/journal/views/index_views.py
@@ -8,3 +8,12 @@ def index(request):
     title = "Home"
     context = {"title": title}
     return render(request, "index.html", context)
+
+
+def design_system(request):
+    """
+    Render design system page
+    """
+    title = "Design System"
+    context = {"title": title}
+    return render(request, "design_system.html", context)

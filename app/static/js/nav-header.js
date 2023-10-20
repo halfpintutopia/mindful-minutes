8a69f93e136000439b512f3f08531f9b42992ccf
Sirinya Richardson <siri.richardson@gmail.com>
2023-10-20 22:03:18 +0200

    style: fix mobile navigation

---
+++ app/static/js/nav-header.js
@@ -0,0 +1,13 @@
+const primaryNavList = document.querySelector('.primary-navigation');
+const navToggleButton = document.querySelector('.mobile-nav-toggle');
+
+navToggleButton.addEventListener('click', function temp() {
+    const visibility = primaryNavList.getAttribute('data-visible');
+    if (visibility === 'false') {
+        primaryNavList.setAttribute('data-visible', 'true');
+        navToggleButton.setAttribute('aria-expanded', 'true');
+    } else {
+        primaryNavList.setAttribute('data-visible', 'false');
+        navToggleButton.setAttribute('aria-expanded', 'false');
+    }
+});

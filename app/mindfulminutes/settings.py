78fe19dc2d0ba46601fb13618bb2b1bb114dba32
Sirinya Richardson <siri.richardson@gmail.com>
2023-10-26 21:04:28 +0200

    chore: finish linting in settings file

--- app/mindfulminutes/settings.py
+++ app/mindfulminutes/settings.py
@@ -158,19 +158,19 @@ if DATABASE_URL:
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".UserAttributeSimilarityValidator",
+        ".UserAttributeSimilarityValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".MinimumLengthValidator",
+        ".MinimumLengthValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".CommonPasswordValidator",
+        ".CommonPasswordValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".NumericPasswordValidator",
+        ".NumericPasswordValidator",
     },
 ]
 

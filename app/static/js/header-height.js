4365539960f1bb46658ade61dba75e80a21e8f88
Sirinya Richardson <halfpint_utopia@icloud.com>
2024-01-25 07:04:11 +0100

    bugfix: add page teaser and fix the scroll pointer

--- app/static/js/header-height.js
+++ app/static/js/header-height.js
@@ -1,14 +1,17 @@
 (function () {
-	const calculateHeaderHeight = () => {
-		const header = document.querySelector('.header-base');
-		const rect = header.getBoundingClientRect();
-		
-		const headerHeight = header.offsetHeight;
-		console.log('heights', header.offsetHeight, header.clientHeight, rect.height);
-		
-		document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
-		
-	};
+	// function calculateHeaderHeight() {
+	console.log('hello');
+	const header = document.querySelector('.header-wrapper');
+	const rect = header.getBoundingClientRect();
 	
-	window.addEventListener('resize', calculateHeaderHeight);
+	const headerHeight = header.offsetHeight;
+	console.log('heights', header.offsetHeight, header.clientHeight, rect.height);
+	
+	document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
+	
+	// }
+	//
+	// addEventListener('resize', () => {
+	// 	console.log("hello");
+	// });
 })();

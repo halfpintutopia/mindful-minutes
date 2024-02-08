ffdb54bdbadfdba2afe24707cacd8534c99f0dcc
Sirinya Richardson <halfpint_utopia@icloud.com>
2024-02-08 14:37:37 +0100

    build: added the delete btn to html for modal

--- app/static/js/schedule.js
+++ app/static/js/schedule.js
@@ -1,5 +1,5 @@
-import {getCurrentDate} from "./helpers/helpers.js";
-import {fetchData, postData} from "./helpers/fetchApi.js";
+import { getCurrentDate } from "./helpers/helpers.js";
+import { fetchData, postData } from "./helpers/fetchApi.js";
 
 const lineHeight = 1;
 const hour = 50;
@@ -23,22 +23,22 @@ const server = 'http://localhost:8008';
 
 let scheduleElement,
 	scheduleEventListElement,
-	scheduleEventListItemsElement,
 	closeBtnElement,
+	deleteBtnElement,
 	modalElement,
 	dateElement,
 	scheduleForm,
 	errorMsgElement,
-	submitBtnElement;
+	buttonGroupElement;
 
 const createTimeOptions = (start, end, el) => {
 	let option;
 	
 	while (start < end) {
 		if (start < 10) {
-			option = new Option(`0${start}:00`, `0${start}:00`);
+			option = new Option(`0${ start }:00`, `0${ start }:00`);
 		} else {
-			option = new Option(`${start}:00`, `${start}:00`);
+			option = new Option(`${ start }:00`, `${ start }:00`);
 		}
 		el.add(option, undefined);
 		
@@ -46,9 +46,14 @@ const createTimeOptions = (start, end, el) => {
 	}
 };
 
-const createEntries = (entries) => {
+const createEntries = async () => {
+	const formData = new FormData(scheduleForm);
+	const currentDate = getCurrentDate();
+	
+	const api = `${ server }/api/users/${ formData.get('user') }/appointments/${ currentDate }/`;
+	const entries = await fetchData(api);
+	
 	entries.map(entry => {
-		console.log(entry);
 		const timeFromArray = entry.time_from.split(':');
 		const timeUntilArray = entry.time_until.split(':');
 		const duration = timeUntilArray[0] - timeFromArray[0];
@@ -59,25 +64,44 @@ const createEntries = (entries) => {
 		item.setAttribute('data-time-until', entry.time_until);
 		item.setAttribute('data-title', entry.title);
 		item.setAttribute('data-entry-id', entry.id);
-		item.style.top = `${timeFromArray[0] * hour - lineHeight}px`;
-		item.style.height = `${duration * hour + lineHeight}px`;
-		item.addEventListener('click', initDialog);
+		item.style.top = `${ timeFromArray[0] * hour - lineHeight }px`;
+		item.style.height = `${ duration * hour + lineHeight }px`;
+		
+		item.addEventListener('click', initForm);
 		scheduleEventListElement.append(item);
 	});
 };
 
-const initSchedule = async () => {
+const showDialog = () => {
+	modalElement.showModal();
+};
+
+const closeDialog = (e) => {
+	e.preventDefault();
+	modalElement.close();
+};
+
+const resetForm = () => {
+	scheduleForm.reset();
+};
+
+const showDeleteBtn = () => {
+	deleteBtnElement.style.visibility = "visible";
+};
+
+const hideDeleteBtn = () => {
+	deleteBtnElement.style.visibility = "hidden";
+};
+
+const initSchedule = () => {
+	
 	let child = scheduleEventListElement.lastElementChild;
 	while (child) {
 		scheduleEventListElement.removeChild(child);
 		child = scheduleEventListElement.lastElementChild;
 	}
-	const formData = new FormData(scheduleForm);
-	const currentDate = getCurrentDate();
 	
-	const api = `${server}/api/users/${formData.get('user')}/appointments/${currentDate}/`;
-	const data = await fetchData(api);
-	createEntries(data);
+	createEntries();
 };
 
 const initTimeSelectElement = () => {
@@ -87,29 +111,28 @@ const initTimeSelectElement = () => {
 	createTimeOptions(1, 25, timeUntilInput);
 };
 
-const initDialog = (e) => {
-	console.log(e.currentTarget);
-	modalElement.showModal();
-	scheduleForm.reset();
+const initForm = (e) => {
+	showDialog();
+	showDeleteBtn();
 	
-	if (e.currentTarget.dataset.title) {
-		const timeFromArray = e.currentTarget.dataset.timeFrom.split(':');
-		const timeUntilArray = e.currentTarget.dataset.timeUntil.split(':');
-		document.querySelector('input#title').value = e.currentTarget.dataset.title;
-		document.querySelector('select#time-from').selectedIndex = timeFromArray[0];
-		document.querySelector('select#time-until').selectedIndex = timeUntilArray[0] - 1;
+	const entry = e.currentTarget;
+	
+	if (entry.hasAttribute('data-entry-id')) {
+		const inputTitle = document.querySelector('input#title');
+		const selectTimeFrom = document.querySelector('select#time-from');
+		const selectTimeUntil = document.querySelector('select#time-until');
 		
-		scheduleForm.setAttribute('data-entry-id', e.currentTarget.dataset.entryId);
-		closeBtnElement.dataset.closeModal = 'delete';
-		closeBtnElement.innerText = 'Delete';
-		
-		closeBtnElement.addEventListener('click', deleteEntry);
+		const timeFromArray = entry.dataset.timeFrom.split(':');
+		const timeUntilArray = entry.dataset.timeUntil.split(':');
+		inputTitle.value = e.currentTarget.dataset.title;
+		selectTimeFrom.selectedIndex = timeFromArray[0];
+		selectTimeUntil.selectedIndex = timeUntilArray[0] - 1;
 	}
-};
 
-const closeDialog = (e) => {
-	e.preventDefault();
-	modalElement.close();
+	
+	// scheduleForm.setAttribute('data-entry-id', e.currentTarget.dataset.entryId);
+	
+
 };
 
 const checkTimes = (data) => {
@@ -131,14 +154,16 @@ const sendData = async (e) => {
 	if (checkTimes(formData)) {
 		let api;
 		if (e.currentTarget.dataset.entryId) {
-			api = `${server}/api/users/${formData.get('user')}/appointments/${currentDate}/${e.currentTarget.dataset.entryId}/`;
+			api = `${ server }/api/users/${ formData.get('user') }/appointments/${ currentDate }/${ e.currentTarget.dataset.entryId }/`;
 			await postData(api, dataObj, formData.get('csrfmiddlewaretoken'), 'PUT');
 			initSchedule();
 		} else {
-			api = `${server}/api/users/${formData.get('user')}/appointments/${currentDate}/`;
+			api = `${ server }/api/users/${ formData.get('user') }/appointments/${ currentDate }/`;
 			await postData(api, dataObj, formData.get('csrfmiddlewaretoken'));
 		}
 		closeDialog(e);
+		hideDeleteBtn();
+		resetForm();
 	} else {
 		errorMsgElement.innerText = "A task or appointment can't finish before it starts, unless you're the Flash?";
 	}
@@ -148,7 +173,7 @@ const deleteEntry = async () => {
 	const formData = new FormData(scheduleForm);
 	const currentDate = getCurrentDate();
 	
-	const api = `${server}/api/users/${formData.get('user')}/appointments/${currentDate}/${scheduleForm.dataset.entryId}/`;
+	const api = `${ server }/api/users/${ formData.get('user') }/appointments/${ currentDate }/${ scheduleForm.dataset.entryId }/`;
 	await postData(api, {}, formData.get('csrfmiddlewaretoken'), 'DELETE');
 	initSchedule();
 	
@@ -157,31 +182,33 @@ const deleteEntry = async () => {
 // https://stackoverflow.com/a/69687500/8614652
 const initDate = () => {
 	const date = new Date();
-	const month = new Intl.DateTimeFormat('en-GB', {month: 'long'});
-	const day = new Intl.DateTimeFormat('en-GB', {day: 'numeric'});
-	const ordinal = new Intl.PluralRules('en-GB', {type: 'ordinal'});
+	const month = new Intl.DateTimeFormat('en-GB', { month: 'long' });
+	const day = new Intl.DateTimeFormat('en-GB', { day: 'numeric' });
+	const ordinal = new Intl.PluralRules('en-GB', { type: 'ordinal' });
 	
-	dateElement.innerHTML = `${week[date.getDay()]}, ${day.format(Date.now())}${suffix[ordinal.select(new Date(Date.now()).getDate())]} <span class="uppercase">${month.format(new Date(Date.now()))} ${date.getFullYear()}</span>`;
+	dateElement.innerHTML = `${ week[date.getDay()] }, ${ day.format(Date.now()) }${ suffix[ordinal.select(new Date(Date.now()).getDate())] } <span class="uppercase">${ month.format(new Date(Date.now())) } ${ date.getFullYear() }</span>`;
 };
 
 const initHtmlElements = () => {
 	scheduleElement = document.querySelectorAll('.schedule__timeline li');
 	scheduleEventListElement = document.querySelector('.event-list');
-	closeBtnElement = document.querySelector('[data-close-modal]');
+	closeBtnElement = document.querySelector('[data-btn="close"]');
 	modalElement = document.querySelector('[data-modal]');
 	dateElement = document.querySelector('.current-date');
 	scheduleForm = document.querySelector('form#schedule');
 	errorMsgElement = document.querySelector('form .error-msg');
+	buttonGroupElement = document.querySelector('.button-group');
+	deleteBtnElement = document.querySelector('[data-btn="delete"]');
 	// submitBtnElement = document.querySelector('button[type="submit"]');
 };
 
 const initEvents = () => {
 	scheduleElement.forEach(el => {
-		el.addEventListener('click', initDialog);
+		el.addEventListener('click', showDialog);
 	});
 	closeBtnElement.addEventListener('click', closeDialog);
 	scheduleForm.addEventListener('submit', sendData);
-	
+	deleteBtnElement.addEventListener('click', deleteEntry);
 };
 
 const init = () => {

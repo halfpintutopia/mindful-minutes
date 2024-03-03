988012ae940ff4f6aa549e75afdeb31ed87ce51a
Sirinya Richardson <halfpint_utopia@icloud.com>
2024-03-03 13:56:24 +0100

    bugfix: add comma in tuple

--- app/mindfulminutes/settings.py
+++ app/mindfulminutes/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -153,8 +154,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = (
-    "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
 )
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
@@ -165,7 +167,7 @@ CLOUDINARY_STORAGE = {
 }
 
 MEDIA_URL = "/media/"
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 

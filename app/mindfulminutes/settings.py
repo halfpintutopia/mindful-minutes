cd271db669149740dd489538149043095efb3d4d
Sirinya Richardson <halfpint_utopia@icloud.com>
2024-03-03 14:36:21 +0100

    bugfix: change whitenoise storage and linting

--- app/mindfulminutes/settings.py
+++ app/mindfulminutes/settings.py
@@ -153,10 +153,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATICFILES_STORAGE = (
-    "whitenoise.storage.CompressedManifestStaticFilesStorage",
-)
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = (
+#     "whitenoise.storage.CompressedManifestStaticFilesStorage",
+# )
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
